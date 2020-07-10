97e24c155e01960508716a4964f170f615463f56
Timothy Byrd <timothy.byrd@laserfiche.com>
2020-07-10 15:26:13 -0700

    Use a SortedDictionary to sort the data schemas

--- SortDataSchemasDocumentFilter.cs
+++ SortDataSchemasDocumentFilter.cs
@@ -10,13 +10,12 @@ namespace swashbuckle_stuff
     using Swashbuckle.AspNetCore.SwaggerGen;
 
     /// <summary>
-    /// Try to sort the data definitions that appear in the "Schemas" section of the Swagger page.
+    /// Sort the data definitions that appear in the "Schemas" section of the Swagger page.
     /// </summary>
     public sealed class SortDataSchemasDocumentFilter : IDocumentFilter
     {
         /// <summary>
-        /// Try to sort the data definitions that appear in the "Schemas" section of the Swagger page.
-        /// This is an ugly way to do it.
+        /// Sort the data definitions that appear in the "Schemas" section of the Swagger page.
         /// </summary>
         /// <param name="openApiDoc">The swagger document</param>
         /// <param name="context">The filter context</param>
@@ -25,27 +24,7 @@ namespace swashbuckle_stuff
             if (openApiDoc == null) throw new ArgumentNullException(nameof(openApiDoc));
             if (context == null) throw new ArgumentNullException(nameof(context));
 
-            var schemaList = new System.Collections.Generic.List<System.Collections.Generic.KeyValuePair<string, OpenApiSchema>>();
-            foreach (var kv in openApiDoc.Components.Schemas)
-            {
-                schemaList.Add(kv);
-            }
-
-            int KvCompare(System.Collections.Generic.KeyValuePair<string, OpenApiSchema> x, System.Collections.Generic.KeyValuePair<string, OpenApiSchema> y)
-            {
-                return string.Compare(x.Key, y.Key, StringComparison.InvariantCulture);
-            }
-
-            schemaList.Sort(KvCompare);
-
-            var newDict = new System.Collections.Generic.Dictionary<string, OpenApiSchema>();
-
-            foreach (var kv in schemaList)
-            {
-                newDict.Add(kv.Key, kv.Value);
-            }
-
-            openApiDoc.Components.Schemas = newDict;
+            openApiDoc.Components.Schemas = new System.Collections.Generic.SortedDictionary<string, OpenApiSchema>(openApiDoc.Components.Schemas);
         }
     }
 }
